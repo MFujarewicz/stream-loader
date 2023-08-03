b0fc88813a6c2ba01bf3602c4b374b9e4128a234
m.fujarewicz <m.fujarewicz@adform.com>
2023-08-03 14:43:57 +0200

    r20

--- stream-loader-tests/src/it/scala/com/adform/streamloader/fixtures/Kafka.scala
+++ stream-loader-tests/src/it/scala/com/adform/streamloader/fixtures/Kafka.scala
@@ -26,7 +26,7 @@ import scala.collection.mutable.ArrayBuffer
 import scala.jdk.CollectionConverters._
 import scala.util.Using
 
-case class KafkaConfig(image: String = "bitnami/kafka:3.4.0-debian-11-r38")
+case class KafkaConfig(image: String = "bitnami/kafka:3.4.0-debian-11-r20")
 
 trait KafkaTestFixture extends Kafka with BeforeAndAfterAll with BeforeAndAfterEach {
   this: Suite with DockerTestFixture =>
